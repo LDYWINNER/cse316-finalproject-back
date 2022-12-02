0a0a525b42d32dc32708409cec8ec88de2b8efc7
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 15:43:50 +0900

    ADD - session implemented

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ app.post('/login', (req, res) => {
             if (result.length > 0) {
                 bcrypt.compare(password, result[0].password, (error, response) => {
                     if (response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
                         res.send(result);
                     } else {
                         res.send({ message: "Wrong username / password combination!" });
