4b317f82c12e11dfaf3cae65f53a79464e568df0
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 14:22:23 +0900

    ADD - cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
 
 const db = mysql.createPool({
     host: 'localhost',
