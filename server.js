6b10defbf871983d03ed35a41bb223a3591af03d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 15:23:53 +0900

    ADD - body-parser, express-session, cookie-parser configured

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require('bcrypt');
 const saltRound = 10;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 app.use(express.json());
 app.use(cors({
@@ -11,6 +14,19 @@ app.use(cors({
     methods: ["GET", "POST"],
     credentials: true,
 }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    session({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24,
+        },
+    })
+);
 
 const db = mysql.createPool({
     host: 'localhost',
