c5bc3186d0929d605f415b2b3fdae61923bf705e
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 18:18:30 +0900

    CHORE

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('livable_personal_company', 'root', 'Dannie1102!*', {
+const db = new Sequelize('daylogger', 'root', 'Dannie1102!*', {
     host: "localhost",
     dialect: "mysql"
 });
