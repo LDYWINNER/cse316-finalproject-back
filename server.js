7b2a9b432313a7f7c4dc2d24d7378effa81aaf7d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 15:14:15 +0900

    ADD - bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 
 app.use(express.json());
 app.use(cors({
@@ -27,30 +29,42 @@ db.getConnection(err => {
 app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    db.execute(
-        "INSERT INTO users (username, password) VALUES (?, ?)",
-        [username, password],
-        (err, result) => {
+    bcrypt.hash(password, saltRound, (err, hash) => {
+        if (err) {
             console.log(err);
         }
-    )
+        db.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            [username, hash],
+            (err, result) => {
+                console.log(err);
+            }
+        );
+    })
 });
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-
     db.execute(
-        "SELECT * FROM users WHERE username = ? AND password = ?",
-        [username, password],
+        "SELECT * FROM users WHERE username = ?;",
+        [username],
         (err, result) => {
             if (err) {
                 res.send({ err: err });
             }
 
             if (result.length > 0) {
-                res.send(result);
-            } else ({ message: "Wrong username/password comination!" });
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (response) {
+                        res.send(result);
+                    } else {
+                        res.send({ message: "Wrong username / password combination!" });
+                    }
+                });
+            } else {
+                res.send({ message: "User doesn't exist" });
+            };
         }
     )
 })
